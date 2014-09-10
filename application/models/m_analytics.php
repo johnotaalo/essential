7c5cdb37fd6101b16f3cc46c91dc078b089fb16c
Rufus Mbugua <mbuguarufus@gmail.com>
2014-09-10 15:32:55 +0300

    Fixed Graph Loading

--- application/models/m_analytics.php
+++ application/models/m_analytics.php
@@ -2419,7 +2419,7 @@ ORDER BY f.fac_county ASC;";
         /**
          * List of Counties that have reported
          */
-        public function getReportingCounties($survey, $survey_category) {
+        public function getReportingCounties() {
             
             /*using CI Database Active Record*/
             try {
