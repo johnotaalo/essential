3066632e71b2ed3b6a41ca8ffaadb6ae23e8998c
Rufus Mbugua <mbuguarufus@gmail.com>
2014-10-09 10:59:43 +0300

    Templating

--- application/modules/template_imci/views/html_close_v.php
+++
@@ -1,8 +0,0 @@
-		<script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
-		<script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-		<script src="<?php echo base_url(); ?>assets/bower_components/fuelux/dist/js/fuelux.min.js"></script>
-		<script src="<?php echo base_url(); ?>assets/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
-		<script src="<?php echo base_url(); ?>assets/bower_components/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-		<script src="<?php echo base_url(); ?>assets/js/script.js"></script>
-	</body>
-</html>
