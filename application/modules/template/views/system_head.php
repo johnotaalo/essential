f37cf0b7248d818170025f6180aeda6c80972c68
Rufus Mbugua <mbuguarufus@gmail.com>
2014-10-30 00:01:28 +0300

    Changed Offline Settings

--- application/modules/template/views/system_head.php
+++ application/modules/template/views/system_head.php
@@ -21,10 +21,34 @@
 <!-- JS -->
 <script src="<?php echo base_url('assets/bower_components/jquery/dist/jquery.js');?>"></script>
 <script src="<?php echo base_url('assets/bower_components/offline/offline.min.js');?>"></script>
-<!--script src="<?php echo base_url('assets/bower_components/offlinejs-simulate-ui/offline-simulate-ui.min.js');?>"></script-->
+<script src="<?php echo base_url('assets/bower_components/offline/js/reconnect.js');?>"></script>
+<script src="<?php echo base_url('assets/bower_components/offline/js/requests.js');?>"></script>
+<script src="<?php echo base_url('assets/bower_components/offline/js/simulate.js');?>"></script>
+<script src="<?php echo base_url('assets/bower_components/offline/js/snake.js');?>"></script>
+<script src="<?php echo base_url('assets/bower_components/offline/js/ui.js');?>"></script>
 <script>
   $(function(){
+Offline.options={
+  // Should we check the connection status immediatly on page load.
+  checkOnLoad: false,
 
+  // Should we monitor AJAX requests to help decide if we have a connection.
+  interceptRequests: true,
+
+  // Should we automatically retest periodically when the connection is down (set to false to disable).
+  reconnect: {
+    // How many seconds should we wait before rechecking.
+    initialDelay: 3
+  },
+
+  // Should we store and attempt to remake requests which fail while the connection is down.
+  requests: true,
+
+  // Should we show a snake game while the connection is down to keep the user entertained?
+  // It's not included in the normal build, you should bring in js/snake.js in addition to
+  // offline.min.js.
+  game: true
+};
         var
             $online = $('.online'),
             $offline = $('.offline');
