77be083a44acc89ddaee136d61293842ce88cfa9
Rufus Mbugua <mbuguarufus@gmail.com>
2014-11-11 10:29:18 +0300

    Added Ionic

---
+++ application/modules/export/controllers/datatable_hanlder.php
@@ -0,0 +1,53 @@
+<?php
+class Datatable_Handler extends MY_Controller{
+  /**
+  * [__construct description]
+  */
+  function __construct()
+  {
+    parent::__construct();
+  }
+  /**
+  * [index description]
+  * @return [type] [description]
+  */
+  public function index()
+  {
+
+  }
+
+  public function normal($data){
+    /**
+    * [$dataSet description]
+    * @var array
+    */
+    $dataSet=array();
+    /**
+    * [$rowCounter description]
+    * @var integer
+    */
+    $rowCounter=1;
+    /**
+    * Looping through rows
+    */
+    foreach($data as $row){
+      /**
+      * [$counter description]
+      * @var integer
+      */
+      $counter=0;
+      /**
+      * Stores a ROW of Data
+      * @var array
+      */
+      $rowSet=array();
+      $rowSet['DT_RowId']='row_'.$rowCounter;
+      foreach($row as $field){
+        $rowSet[]=array($counter=>$field);
+        $counter++;
+      }
+
+      $dataSet[]=$rowSet;
+    }
+    return $dataSet;
+  }
